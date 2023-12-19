d5898a1fedc17af0cb54cd63a8f62130f1779e61
fate4gle <37401377+fate4gle@users.noreply.github.com>
2023-12-19 12:12:20 +0100

    Transceivers Updated
    UDP and Serial Transceiver Development initated

---
+++ UnityComponents/Communication/Serial/SerialClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DTStacks.Communication.Serial;
+using UnityEngine;
+
+namespace DTStacks.UnityComponents.Communication.Serial
+{
+    /// <summary>
+    /// SerialClient to be used as a transceiving module
+    /// </summary>
+    public class SerialClient : SerialCommunication
+    {
+
+        /// <summary>
+        /// Method to define what happens when Data is received and preprocessed to a string
+        /// </summary>
+        /// <param name="message"> the received message as a string</param>
+        public override void ProcessMessage(string message)
+        {
+            
+        }
+
+    }
+}
